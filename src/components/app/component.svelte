301c629b3975ceb00b7b505d18aba0661b3cbd9c
Antony Jones <aj@desirableobjects.co.uk>
2018-05-07 11:35:48 +0100

    reinstate real component

--- src/components/app/component.svelte
+++ src/components/app/component.svelte
@@ -1,1 +1,35 @@
-<h1>hi</h1>
+<p>
+  Hello { name }, the time is <span class="the-time">{ hours }:{ minutes }:{ seconds }</span>
+</p>
+
+<style>
+  .the-time {
+    font-weight: bold;
+  }
+</style>
+
+<script>
+  export default {
+    data () {
+			return {
+				time: new Date()
+			}
+		},
+
+		computed: {
+			hours: ({ time }) => time.getHours(),
+			minutes: ({ time }) => time.getMinutes(),
+			seconds: ({ time }) => time.getSeconds()
+		},
+
+		oncreate () {
+			this.interval = setInterval( () => {
+				this.set({ time: new Date() })
+			}, 1000 )
+		},
+
+		ondestroy () {
+			clearInterval( this.interval )
+		}
+  }
+</script>>
